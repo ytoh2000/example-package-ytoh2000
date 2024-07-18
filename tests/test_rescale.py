04d0658c7af872ce752c57dd1442c95477f6d6a2
Yongtaek Oh <53324042+ytoh2000@users.noreply.github.com>
2024-07-18 14:30:28 +0000

    test

---
+++ tests/test_rescale.py
@@ -0,0 +1,10 @@
+# contents of tests/test_rescale.py
+import numpy as np
+from example_package_ytoh2000.rescale import rescale
+
+
+def test_rescale():
+    np.testing.assert_allclose(
+        rescale(np.linspace(0, 100, 5)),
+        np.array([0.0, 0.25, 0.5, 0.75, 1.0]),
+    )
